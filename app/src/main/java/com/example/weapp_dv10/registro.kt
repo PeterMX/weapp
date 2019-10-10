093e7c67185002ee11d403f0fcd29268e793d194
Uriel <urychdz@hotmail.es>
2019-10-10 16:15:19 -0500

    initial commit

---
+++ app/src/main/java/com/example/weapp_dv10/registro.kt
@@ -0,0 +1,12 @@
+package com.example.weapp_dv10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class registro : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
